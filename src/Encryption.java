00bfc06bd73531c0656306039c0a8643f74399ce
VictorSchweitz <victorschweitz@hotmail.com>
2021-09-22 10:12:24 +0200

    Initiated commit

---
+++ src/Encryption.java
@@ -0,0 +1,16 @@
+public class Encryption
+{
+    public static void main(String[] args)
+    {
+        // 1
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int charIndex = alphabet.indexOf(alphabet) + 1;
+
+    }
+
+
+    public static char characterToIndex(char charIndex)
+    {
+        return 0;
+    }
+}
